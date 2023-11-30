db1d11c5967475a98b86b30efce61e34e788541d
velolala <fiets@einstueckheilewelt.de>
2023-11-30 12:40:25 +0100

    Cleanup initial draft

--- canvas_monitor.py
+++ canvas_monitor.py
@@ -1,7 +1,18 @@
 from nanoleafapi import Nanoleaf
+from time import sleep
 from pygame import Surface
 import socket
-from nanoleafapi import RED, ORANGE, YELLOW, GREEN, LIGHT_BLUE, BLUE, PINK, PURPLE, WHITE
+from nanoleafapi import (
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    LIGHT_BLUE,
+    BLUE,
+    PINK,
+    PURPLE,
+    WHITE,
+)
 
 BLACK = (0, 0, 0)
 
@@ -11,8 +22,8 @@ NANOLEAF_UDP_PORT = 60222
 
 class NanoleafDisplay():
 
-    def __init__(self, nl: Nanoleaf):
-        
+    def __init__(self, nl: Nanoleaf, hello=True):
+
         self.nanoleaf_socket = socket.socket(
             socket.AF_INET, socket.SOCK_DGRAM, 0
         )
@@ -25,19 +36,37 @@ class NanoleafDisplay():
 
         self.n_panels_b = n_panels.to_bytes(2, "big")
         self.nl.enable_extcontrol()
-    
+        if hello:
+            self.hello()
+
+    def hello(self):
+        colors = (
+            BLACK, RED, ORANGE, YELLOW, GREEN,
+            LIGHT_BLUE, BLUE, PINK, PURPLE, WHITE,
+        )
+        for step in range(len(self.panel_ids)):
+            frame_data = [
+                colors[(step + i) % len(colors)]
+                for i in range(len(self.panel_ids))
+            ]
+            self.draw_frame(frame_data)
+            sleep(3. / len(self.panel_ids))
+        self.draw_frame([BLACK for _ in range(len(self.panel_ids))])
+
     def close(self):
         self.nanoleaf_socket.close()
 
     def blit(self, s: Surface):
         pixels = []
         for y in range(s.get_height()):
-          pixels.extend([s.get_at((x, y))[:3] for x in range(s.get_width())])
+            pixels.extend([s.get_at((x, y))[:3] for x in range(s.get_width())])
         self.draw_frame(pixels)
 
     def draw_frame(self, frame_data):
         transition = 0
-        assert len(frame_data) == len(self.panel_ids), (len(frame_data), len(self.panel_ids))
+        assert len(frame_data) == len(self.panel_ids), (
+            len(frame_data), len(self.panel_ids)
+        )
 
         send_data = b""
         send_data += self.n_panels_b
--- test_monitor.py
+++ test_monitor.py
@@ -1,13 +1,15 @@
 import pygame as pg
 import sys
 import signal
-from canvas_monitor import RED, GREEN, BLACK, Nanoleaf, NanoleafDisplay
+from canvas_monitor import Nanoleaf, NanoleafDisplay
 from time import sleep
 import random
+
 
 def signal_handler(sig, frame):
     display.close()
     sys.exit(0)
+
 
 signal.signal(signal.SIGINT, signal_handler)
 
@@ -20,45 +22,38 @@ s.fill("black")
 s.set_colorkey("black")
 
 
-#viewport = s.subsurface((0, 3, 12, 6))
-#print(viewport.get_size())
-#display.blit(viewport)
-
 def surface_rotate(s):
-   red = s.copy()
-   pg.draw.line(red, "red", (0, 0), (12, 6), width=2)
-   green = s.copy()
-   pg.draw.line(green, "green", (0, 0), (12, 6), width=2)
-   yellow = s.copy()
-   pg.draw.line(yellow, "yellow", (0, 0), (12, 6), width=2)
+    red = s.copy()
+    pg.draw.line(red, "red", (0, 0), (12, 6), width=2)
+    green = s.copy()
+    pg.draw.line(green, "green", (0, 0), (12, 6), width=2)
+    yellow = s.copy()
+    pg.draw.line(yellow, "yellow", (0, 0), (12, 6), width=2)
 
-   line = red
+    line = red
 
-   angle = 0
-   d_angle = 1
-   scroll = 0
-   direction = 1
-   while True:
-       if random.random() > .99:
-         direction = random.choice((-1, 1))
-         d_angle += direction
-         line = random.choice((red, green, yellow))
-       angle = (angle + (d_angle * direction)) % 360
-       scroll = (scroll + 1) % 12
-       sleep(.005)
-       s.fill("black")
-       s = pg.transform.rotate(line, angle)
-       x = int((s.get_width() - 12) / 2 + .5)
-       y = int((s.get_height() - 6) / 2 + .5)
-       print(x, y, s.get_size())
-       viewport = s.subsurface((x, y, 12, 6))
-       display.blit(viewport)
-
-def position_rotate(s):
     angle = 0
     d_angle = 1
     scroll = 0
     direction = 1
+    while True:
+        if random.random() > .99:
+            direction = random.choice((-1, 1))
+        d_angle += direction
+        line = random.choice((red, green, yellow))
+        angle = (angle + (d_angle * direction)) % 360
+        scroll = (scroll + 1) % 12
+        sleep(.005)
+        s.fill("black")
+        s = pg.transform.rotate(line, angle)
+        x = int((s.get_width() - 12) / 2 + .5)
+        y = int((s.get_height() - 6) / 2 + .5)
+        print(x, y, s.get_size())
+        viewport = s.subsurface((x, y, 12, 6))
+        display.blit(viewport)
+
+
+def position_rotate(s):
     sx, sy, ex, ey = (0, 0, 12, 6)
     while True:
         line = s.copy()
@@ -71,5 +66,5 @@ def position_rotate(s):
         ey = (ey + 1)
 
 
-#surface_rotate(s)
+# surface_rotate(s)
 position_rotate(s)
