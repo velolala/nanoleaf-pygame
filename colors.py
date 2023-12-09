70ee689468f11084d381b87c7a1a5ed9ea687b48
velolala <fiets@einstueckheilewelt.de>
2023-12-10 00:12:03 +0100

    Add color module

--- colors.py
+++ colors.py
@@ -9,6 +9,7 @@ from nanoleafapi import (
     PURPLE,
     WHITE,
 )
+from collections import namedtuple
 
 BLACK = (0, 0, 0)
 
@@ -25,3 +26,22 @@ COLORS = [
     WHITE,
 ]
 palette = COLORS
+
+RGBA = namedtuple("RGBA", ["r", "g", "b", "a"])
+
+TRANSLUCENT = RGBA(0, 0, 0, 0)
+RGBA_COLORS = [TRANSLUCENT]
+
+for color in COLORS:
+    RGBA_COLORS.append(RGBA(*color, 255))
+
+
+def make_background(dimensions, color):
+    x, y = dimensions
+    return [
+        [color for _x in range(x)]
+        for _y in range(y)
+    ]
+
+
+BLUE_BACK = make_background((12, 6), RGBA(0, 0, 255, 255))
