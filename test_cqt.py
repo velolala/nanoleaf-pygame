2bba3396429be1f257f81cc1058287c4ea39833a
velolala <fiets@einstueckheilewelt.de>
2024-02-07 22:57:40 +0100

    Experiment with cqt filters
    for visualisation purposes, it seems better without filtering.

--- test_cqt.py
+++ test_cqt.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 
 sr_44 = 44100
 sr_22 = 22500
-sr_22 = sr_44
 block_duration = 22
 
 
@@ -23,29 +22,66 @@ def get_data(num=1):
         return y
 
 
-def calc_cqt(y):
-    print(y, y.shape)
+def calc_cqt(y, sr=sr_44, harm=True):
+    if harm:
+        harm_y = librosa.effects.harmonic(y, margin=8)
+    # print(y, y.shape)
+    fmin = librosa.note_to_hz("C2")
     C = np.abs(
         librosa.cqt(
             y,
-            sr=sr_22,
-            fmin=librosa.note_to_hz("C4"),
+            sr=sr,
+            fmin=fmin,
             bins_per_octave=12,
-            n_bins=12,
+            n_bins=12 * 6,
         )
     )
-    print("CQT")
-    fig, ax = plt.subplots()
+    C_harm = np.abs(
+        librosa.cqt(
+            harm_y,
+            sr=sr,
+            fmin=fmin,
+            bins_per_octave=12,
+            n_bins=12 * 6,
+        )
+    )
+
+    chroma_filter = np.minimum(
+        C_harm,
+        librosa.decompose.nn_filter(
+            C_harm, aggregate=np.median, metric="cosine"
+        ),
+    )
+
+    print("CQT" + ("harm" if harm is True else ""))
+    fig, ax = plt.subplots(nrows=3, sharex=True, sharey=True)
     try:
-        img = librosa.display.specshow(
+        img1 = librosa.display.specshow(
             abs(C),
-            sr=sr_22,
+            sr=sr,
             x_axis="time",
             y_axis="cqt_note",
-            ax=ax,
+            ax=ax[0],
         )
-        ax.set_title("Constant-Q power spectrum")
-        fig.colorbar(img, ax=ax, format="%+2.0f dB")
+        img2 = librosa.display.specshow(
+            abs(C_harm),
+            sr=sr,
+            x_axis="time",
+            y_axis="cqt_note",
+            ax=ax[1],
+        )
+        img3 = librosa.display.specshow(
+            abs(C_harm),
+            sr=sr,
+            x_axis="time",
+            y_axis="cqt_note",
+            ax=ax[2],
+        )
+        ax[0].set_title("Constant-Q power spectrum")
+        ax[1].set_title("Harmonic Constant-Q power spectrum")
+        fig.colorbar(img1, ax=ax, format="%+2.0f dB")
+        fig.colorbar(img2, ax=ax, format="%+2.0f dB")
+        fig.colorbar(img3, ax=ax, format="%+2.0f dB")
         plt.show()
     except Exception as ex:
         plt.close()
@@ -53,13 +89,15 @@ def calc_cqt(y):
 
 
 if __name__ == "__main__":
-    frames = 200
-    calc_cqt(librosa.load(librosa.ex("trumpet"))[0])
+    frames = 100000
+    # calc_cqt(librosa.load(librosa.ex("trumpet"))[0], sr=sr_22)
+    import time
+
     while True:
         try:
             print(frames)
+            time.sleep(1)
             y = get_data(frames)
             calc_cqt(y)
         except librosa.util.exceptions.ParameterError as ex:
             print(ex)
-        frames *= 2
